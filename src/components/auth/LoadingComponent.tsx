8a18a1254942f2f0583a5be702505601088b61ba
Loïc Délon <l.delon@proton.me>
2025-12-29 19:05:38 +0100

    rev: loading behaviour

--- src/components/auth/LoadingComponent.tsx
+++ src/components/auth/LoadingComponent.tsx
@@ -2,7 +2,7 @@ const LoadingComponent: React.FC = () => {
     return (
         <div style={{ textAlign: "center", padding: "50px" }}>
             <h1>Chargement...</h1>
-            <div>Chargement de l'authentification...</div>
+            <div>Authentification...</div>
         </div>
     );
 };
